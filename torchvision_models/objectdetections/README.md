d69a912a36de37ec50e0b8b2928fead6b215a0ac
Masahiro Hiramori <mhg00g13@gmail.com>
2024-07-23 18:11:39 +0900

    object detection

---
+++ torchvision_models/objectdetections/README.md
@@ -0,0 +1,27 @@
+# Object Detection Models from Torchvision
+
+## Run
+
+```bash
+python objectdetection_test.py --model MODEL_NAME
+
+# example
+python objectdetection_test.py --model fasterrcnn_resnet50_fpn
+```
+
+## Supporting matrix
+
+| Model                                 | Status  |
+| ------------------------------------- | ------- |
+| fasterrcnn_mobilenet_v3_large_320_fpn | not yet |
+| fasterrcnn_mobilenet_v3_large_fpn     | not yet |
+| fasterrcnn_resnet50_fpn               | not yet |
+| fasterrcnn_resnet50_fpn_v2            | not yet |
+| fcos_resnet50_fpn                     | not yet |
+| keypointrcnn_resnet50_fpn             | not yet |
+| maskrcnn_resnet50_fpn                 | not yet |
+| maskrcnn_resnet50_fpn_v2              | not yet |
+| retinanet_resnet50_fpn                | not yet |
+| retinanet_resnet50_fpn_v2             | not yet |
+| ssd300_vgg16                          | not yet |
+| ssdlite320_mobilenet_v3_large         | not yet |
--- torchvision_models/objectdetections/objectdetection_test.py
+++ torchvision_models/objectdetections/objectdetection_test.py
@@ -4,9 +4,12 @@ import torch
 import torch.fx
 import torchvision
 from torchvision.io import read_image
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
 import tvm
 from tvm import relax
 from tvm.relax.frontend.torch import from_fx
+from PIL import Image
 
 
 def main(args: argparse.Namespace) -> None:
@@ -14,7 +17,9 @@ def main(args: argparse.Namespace) -> None:
 
   weights_enum = torchvision.models.get_model_weights(model_name)
   weights: torchvision.models.Weights = weights_enum.DEFAULT
-  torch_model = torchvision.models.get_model(model_name, weights=weights).eval()
+  torch_model = torchvision.models.get_model(
+    model_name, weights=weights, box_score_thresh=0.9
+  ).eval()
 
   preprocess = weights.transforms()
 
@@ -22,7 +27,21 @@ def main(args: argparse.Namespace) -> None:
   preprocessed_img = preprocess(img)
   batch: torch.Tensor = preprocessed_img.unsqueeze(0)
 
-  output_torch: torch.Tensor = torch_model(batch)
+  output_torch: torch.Tensor = torch_model(batch)[0]
+  labels = [weights.meta["categories"][i] for i in output_torch["labels"]]
+  box = draw_bounding_boxes(
+    img,
+    boxes=output_torch["boxes"],
+    labels=labels,
+    colors="red",
+    width=4,
+    font="NotoMono-Regular.ttf",
+    font_size=30,
+  )
+  to_pil_image(box).save("torch_grace_hopper_517x606.jpg")
+
+  # tvm
+  graph_model: torch.fx.GraphModule = torch.fx.symbolic_trace(torch_model)
 
 
 if __name__ == "__main__":
